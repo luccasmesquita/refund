83c5d16e2b99c201340e2dbe79bb5212eaa3ac4b
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2025-03-25 19:08:49 +0000

    Formatando a moeda

--- scripts.js
+++ scripts.js
@@ -1,7 +1,24 @@
 //Seleciona os elementos do formulario
 const amount = document.getElementById("amount")
 
+//captura o evento de input para formatar o valor
 amount.oninput = () => {
+    //obtem o valor atual do input e remove os caracteres não numericos
     let value = amount.value.replace(/\D/g, "")
-    amount.value = value
+
+    //transformar o valor em centavos 
+    value = Number(value) / 100
+
+    //atualiza o valor do input
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+    //formata o valor no padrão BRL (Real brasileiro)
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    })
+
+    return value
 }
