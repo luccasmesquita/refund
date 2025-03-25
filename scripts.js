d3b2b810f757619cdf8ee847c9b66e46dec07cdc
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2025-03-25 23:50:26 +0000

    Criando um objeto de nova despesa

--- scripts.js
+++ scripts.js
@@ -27,6 +27,17 @@ function formatCurrencyBRL(value) {
     return value
 }
 
+//captura o evento de submit fo formulario para obter os valores
 form.onsubmit = (event) => {
     event.preventDefault() // não recarrega a pagina 
+
+    //cria um objeto com os detalhes na nova despesa
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
 }
